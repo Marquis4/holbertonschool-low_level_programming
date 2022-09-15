b7fb09d0269da7ed65d6a823491cd88cfd945d41
Marquis <marquistumey12@gmail.com>
2022-09-15 09:47:54 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
-#include <stdio.h>                                                                                                                        
-/**                                                                                                                                       
- * main - Entry point                                                                                                                     
- * Return: Always 0 (Success                                                                                                              
- */                                                                                                                                       
-int main(void)                                                                                                                            
-{                                                                                                                                         
-        puts("\"with proper grammar, but the outcome is a piece of art,");                                                                     
-        return (0);                                                                                                                       
-}                                                            
+#include <studio.h>
+main ()
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
+}
