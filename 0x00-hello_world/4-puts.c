c5ec3876fc0e46d63deb537db2dc60d550df008f
Marquis <marquistumey12@gmail.com>
2022-09-15 08:59:28 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #!/bin/bash
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
+$ ./4-puts
+"Programming is like building a multilingual puzzle
+
+$
