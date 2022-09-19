2c9e8b3ffb091ab752cd1969fed6b838db8088a1
Marquis <marquistumey12@gmail.com>
2022-09-19 11:16:49 -0700

    C1

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	for (n = '0'; n <= '9'; n++)
-	{
-	       putchar(n);
-	}
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
 
-	for (n = 'a'; n <= 'f'; n++)
-	{
-		putchar(n);
-	}
+for (n = 'a'; n <= 'f'; n++)
+{
+putchar(n);
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
