5c7fdb7b504fae9180369035538da9eea48309ad
Marquis <marquistumey12@gmail.com>
2022-09-19 09:22:29 -0700

    C1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
