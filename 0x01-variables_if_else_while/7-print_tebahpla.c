3b33483c539becf87242fa3342c2f586eba9a2a1
Marquis <marquistumey12@gmail.com>
2022-09-19 10:09:12 -0700

    C1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Reverse alphabets
+ *
+ * Returns: Always 0.
+ */
+int main(void)
+{
+	int i;
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	return (0);
+}
