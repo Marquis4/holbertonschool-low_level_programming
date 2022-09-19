bd49ecfae79597088ed715f62d1bd3cd95c32ba0
Marquis <marquistumey12@gmail.com>
2022-09-19 08:28:05 -0700

    C1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,6 @@ int main(void)
 {
 	char i;
 
-	printf("Capital (upper) case characters:\n");
-	for (i = 'A'; i <= 'Z'; i++)
-		printf("%c ", i);
-
 	printf("\n\nLower case characters:\n");
 	for (i = 'a'; i <= 'z'; i++)
 		printf("%c ", i);
