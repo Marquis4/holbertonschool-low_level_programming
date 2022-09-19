2536d47ee68c3a10038eb94f15f3a20bd235d3ae
Marquis <marquistumey12@gmail.com>
2022-09-19 08:52:51 -0700

    C1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char k
+
+	for (k = 'A'; k <= 'Z'; k++)
+		putchar(k)
+
+		putchar('\n');
+		return (0);
+}
