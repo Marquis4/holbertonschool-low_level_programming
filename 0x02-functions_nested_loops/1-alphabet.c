f9300c887a06b764aa1c4ac6fc192a23e6384946
Marquis <marquistumey12@gmail.com>
2022-09-20 07:21:57 -0700

    C2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a';  i<= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
