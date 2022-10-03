238425038aa549d4b4e2116db4ce4cc435b094df
Marquis <marquistumey12@gmail.com>
2022-10-03 08:51:12 -0700

    C6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Changes all lowercase
+ * @p: The string will be modified
+ *
+ * Return: char var
+ */
+char *string_toupper(char *)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
