9aed77583e775ae678fe572515bf722595c8bba1
Marquis <marquistumey12@gmail.com>
2022-11-15 18:59:44 -0800

    C18

--- 0x18-doubly_linked_lists/list.h
+++ 0x18-doubly_linked_lists/list.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -27,3 +30,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
