494a7fcb7b670e6b5d3152ca2a0711a71a6f93c9
Marquis4 <marquistumey12@gmail.com>
2023-03-20 19:51:04 -0700

    C13

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * * list_len - returns the number of elements within a list
- * * @h:singly linked list
- * * Return: num of elements in list
- * */
+ * list_len - returns the number of elements within a list
+ * @h:singly linked list
+ * Return: num of elements in list
+ */
 size_t list_len(const list_t *h)
 {
 	const list_t *temp = h;
